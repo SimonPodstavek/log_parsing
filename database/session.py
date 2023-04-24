5ae4d4d045f891adacb281ba13e7f4c6ebbd266a
SimonPodstavek <s.podstavek2@gmail.com>
2023-04-24 08:51:56 +0200

    added another remote clusters and one local cluster

--- database/session.py
+++ database/session.py
@@ -3,7 +3,15 @@ from pymongo import MongoClient
 
 
 def create_session():
-    cluster = "mongodb+srv://spodstavek:{}@cluster0.1ctzpcl.mongodb.net/implementation_log?retryWrites=true&w=majority".format(os.getenv("HMH_MONGO_LOG_PSSWD"))
+    #127.0.0.1
+    cluster = "mongodb://localhost:27017"
+
+
+    #Atlas
+    # cluster = "mongodb+srv://spodstavek:{}@cluster0.1ctzpcl.mongodb.net/implementation_log?retryWrites=true&w=majority".format(os.getenv("HMH_MONGO_LOG_PSSWD"))
+    
+    #Azure
+    # cluster = "mongodb://free-test:{}@free-test.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@free-test@".format(os.getenv("AZURE_MONGO_LOG_PSSWH"))
     client = MongoClient(cluster)
     db = client.implementation_log
     return db
