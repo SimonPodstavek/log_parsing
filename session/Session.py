3715a18131efb9d0c8acefdb09e05dc2f7c1e865
SimonPodstavek <s.podstavek2@gmail.com>
2023-06-25 14:43:11 +0200

    New file structure

---
+++ session/Session.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+
+
+
+def create_session():
+    try:
+        conn=psycopg2.connect(
+            host="postgretst.postgres.database.azure.com",
+            port="5432",
+            database="implementation_log",
+            user="ovypt1",
+            password=os.getenv("HMH_AZURE_LOG_PSSWD")
+        )
+
+        cursor = conn.cursor()
+        return cursor, conn
+    except:
+        print("Chyba 109: Nepodarilo sa utvoriť reláciu medzi databázou a klientom.")
+        exit()
